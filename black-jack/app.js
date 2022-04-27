59ab3045d87477fcab43cab6c93cafbb52d43ef0
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-27 10:44:23 +0300

    added second button for a new card

--- black-jack/app.js
+++ black-jack/app.js
@@ -5,15 +5,24 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageElement = document.getElementById('message-el');
+let sumElement = document.getElementById('sum-el');
+let cardsElement = document.getElementById('cards-el');
 function startGame(){
+    sumElement.textContent = `Sum: ${sum}`;
+    cardsElement.textContent = `Cards: ${firstCard} ${secondCard}`;
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)
+    messageElement.textContent = message;
+}
+
+function newCard(){
+    
 }
